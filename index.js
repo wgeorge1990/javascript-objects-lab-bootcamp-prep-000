2038ad1a0066dfea0f6adb6de03e8358ded842c7
william george <williameverettgeorge@icloud.com>
2018-09-23 23:40:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,3 +19,7 @@ function deleteFromObjectByKey(object, key) {
   delete object.key;
   return object;
 }
+function destructivelyDeleteFromObjectByKey(object, key) {
+delete object[key]
+return object
+}
