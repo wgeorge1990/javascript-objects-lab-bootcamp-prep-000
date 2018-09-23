b32fae6b3ebb82e7216f4c44d71ed2bba01dab95
william george <williameverettgeorge@icloud.com>
2018-09-23 23:47:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,11 +10,7 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
   object[key] = value;
   return object;
 }
-function deleteFromObjectByKey(object, key) {
-  var newObj = Object.assign({}, object);
-  delete newObj.key;
-  return newObj;
-}
+
  function destructivelyDeleteFromObjectByKey(object, key) {
   delete object.key;
   return object;
@@ -22,4 +18,9 @@ function deleteFromObjectByKey(object, key) {
 function destructivelyDeleteFromObjectByKey(object, key) {
 delete object[key]
 return object
+}
+function deleteFromObjectByKey(object, key) {
+  var clone = Object.assign({}, object);
+  delete clone[key];
+  return clone;
 }
